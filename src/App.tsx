3c89ba23ddd2718897df0859eb76f999950f4af8
Toru Yamaguchi <toruyama25@icloud.com>
2025-09-02 22:37:41 +0100

    prettier

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,11 @@ import { MeshStandardMaterial } from "three";
 function App() {
   return (
     <>
-      <Canvas 
-        dpr={[1,2]} 
+      <Canvas
+        dpr={[1, 2]}
         camera={{ fov: 70, position: [0, 2, 15] }}
         style={{ width: "100vw", height: "100vh" }}
-        >
+      >
         <color attach="background" args={["#ffffff"]} />
 
         <ambientLight intensity={0.5} />
@@ -18,11 +18,8 @@ function App() {
 
         <group position={[0, -1.5, 0]}>
           <mesh rotation={[-Math.PI / 2, 0, 0]}>
-            <planeGeometry args={[50,50]} />
-            <meshStandardMaterial
-              color="#71787b"
-              roughness={0.9}
-            />
+            <planeGeometry args={[50, 50]} />
+            <meshStandardMaterial color="#71787b" roughness={0.9} />
           </mesh>
         </group>
         <Environment preset="sunset" />
@@ -30,8 +27,5 @@ function App() {
     </>
   );
 }
-
-
-
 
 export default App;
