c4cbd5a0905e42865ff06dedd86e194059f6f97d
Toru Yamaguchi <toruyama25@icloud.com>
2026-06-23 23:48:15 +0100

    fixing url issue with hashRouter otherwise you can't jump to url

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import "./index.css";
 import HomePage from "./pages/HomePage.tsx";
 
@@ -11,8 +11,8 @@ import HomePage from "./pages/HomePage.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <HomePage />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 );
