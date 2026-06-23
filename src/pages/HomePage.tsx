f3b7a8fb265beeda5e1f2c0b102217f2f88a19c7
Toru Yamaguchi <toruyama25@icloud.com>
2026-06-23 23:48:41 +0100

    fixing hompage semantic error

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route, HashRouter, Link } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
 import ProjectsPage from "./Projects";
 import InterestsPage from "./Interests";
 import BlogHomePage from "./BlogHome";
